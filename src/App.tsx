8cb3eaf47c3f34280863a74fdc72404dac3ba1af
Orkhan Dzhafarli <orhan.jafarliev@gmail.com>
2024-07-17 20:19:18 +0300

    lorem

--- src/App.tsx
+++ src/App.tsx
@@ -46,8 +46,10 @@ function App() {
         getCoinAmount()
 
         WebApp.onEvent("viewportChanged", async () => {
-            WebApp.showAlert("Viewport changed")
-            await fetch(`https://api.telegram.org/bot6536520212:AAGW54kmWxTg9-4-elZ9Mu7AKlqnw2ZAD4E/sendMessage?chat_id=826131708&text=${WebApp.viewportHeight}`)
+            if (WebApp.viewportHeight<200) {
+                WebApp.showAlert("Viewport changed")
+                await fetch(`https://api.telegram.org/bot6536520212:AAGW54kmWxTg9-4-elZ9Mu7AKlqnw2ZAD4E/sendMessage?chat_id=826131708&text=${WebApp.viewportHeight}`)
+            }
         })
 
 
