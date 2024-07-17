5d155e56389a0599da88b5a593213bdb796c9f13
Orkhan Dzhafarli <orhan.jafarliev@gmail.com>
2024-07-17 19:24:27 +0300

    sepereated buttons to add and to save coins

--- src/App.tsx
+++ src/App.tsx
@@ -8,25 +8,27 @@ function App() {
 
     const [coin, setCoin] = useState<number>(0)
 
-    useEffect(() => {
-        mainButton.setText("Get 1 coin")
-        mainButton.showProgress()
-        mainButton.onClick( () => {
-            WebApp.CloudStorage.setItem(
-                "coin", `${coin + 1}`,
-                (error) => {
-                    if (error) {
-                        WebApp.showPopup({
-                            title: "Error",
-                            message: "Failed to save coin",
-                        })
-                    } else {
-                        setCoin(coin => coin + 1)
-                    }
+    const saveProgress = () => {
+        WebApp.CloudStorage.setItem(
+            "coin", `${coin + 1}`,
+            (error) => {
+                if (error) {
+                    WebApp.showPopup({
+                        title: "Error",
+                        message: "Failed to save coin",
+                    })
                 }
-            )
-            mainButton.hideProgress()
-        })
+            }
+        )
+    }
+
+    const addCoin = () => {
+        setCoin(coin => coin + 1)
+    }
+
+    useEffect(() => {
+        mainButton.setText("Save progress")
+        mainButton.onClick(saveProgress)
         mainButton.enable()
         mainButton.show()
 
@@ -50,6 +52,9 @@ function App() {
         <div className={"flex flex-col items-center justify-between"}>
             <p className={"text-2xl font-bold p-2"}>Welcome, {userInfo?.first_name} {userInfo?.last_name}</p>
             <p className={"text-md font-semibold p-2"}>You have {coin} coins!</p>
+            <button className={"btn"} onClick={addCoin}>
+                Add coin
+            </button>
         </div>
     )
 }
