9c2f2e9225a204fe6ac1e65a529257981eadf9a2
Orkhan Dzhafarli <orhan.jafarliev@gmail.com>
2024-07-17 18:22:11 +0300

    added TW and TWA SDK

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import WebApp from "@twa-dev/sdk";
+
+WebApp.ready()
+WebApp.expand()
+// WebApp.enableClosingConfirmation()
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
     <App />
-  </React.StrictMode>,
 )
